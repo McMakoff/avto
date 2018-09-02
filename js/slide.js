b86941c52671098006bd659b20c9452f4ce1dba5
McMakoff <38282022+McMakoff@users.noreply.github.com>
2018-09-02 14:04:56 +0600

    Add files via upload

---
+++ js/slide.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($){
+	$('.slide .slick-dots li button').html('<span class="btn-text">tayota camry</span><span class="btn-arrow"></span>');
+	$('.slide .slick-dots li:nth-child(2) button .btn-text').text("kia rio");
+	$('.slide .slick-dots li:nth-child(3) button .btn-text').text("Renault logan");
+	$('.slide .slick-dots li:nth-child(4) button .btn-text').text("Range Rover");
+	$('.slide .slick-dots li:nth-child(5) button .btn-text').text("Mercedes S class");
+	$('.slide .slick-dots li:nth-child(6) button .btn-text').text("hyundai solaris");
+	$('.slide .slick-dots li:nth-child(7) button .btn-text').text("Mercedes V class");
+	$('.slide .slick-dots li:nth-child(8) button .btn-text').text("Ford Focus");
+	$('.slide .slick-dots li:nth-child(9) button .btn-text').text("Ford Mondeo");
+	$('.slide .slick-dots li:nth-child(10) button .btn-text').text("BMW 5 Series");
+
+});
